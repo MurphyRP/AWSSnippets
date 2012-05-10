66c6270814d75bba27493f16d844db8d85f5dcd3
Rob Murphy <bearmurph@gmail.com>
2012-05-10 11:45:44 -0400

    Basic first commit  - nothing to see here

---
+++ AWSSnippetsTests/AWSSnippetsTests.h
@@ -0,0 +1,13 @@
+//
+//  AWSSnippetsTests.h
+//  AWSSnippetsTests
+//
+//  Created by Bear Murphy on 5/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AWSSnippetsTests : SenTestCase
+
+@end
---
+++ AWSSnippetsTests/AWSSnippetsTests.m
@@ -0,0 +1,32 @@
+//
+//  AWSSnippetsTests.m
+//  AWSSnippetsTests
+//
+//  Created by Bear Murphy on 5/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AWSSnippetsTests.h"
+
+@implementation AWSSnippetsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AWSSnippetsTests");
+}
+
+@end
